83bd2824550c3689e54fd5f31f76e01cb09fd45b
MPechorin <maksimp@wix.com>
2014-12-28 18:53:16 +0200

    done

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,26 +1,71 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (let [pow' (fn [acc n]
+               (if (zero? n)
+                 acc
+                 (recur (* acc base) (dec n))))]
+    (pow' 1 exp)))
 
 (defn last-element [a-seq]
-  ":(")
+  (let [last' (fn [lst a-seq]
+                (if (empty? a-seq)
+                  lst
+                  (recur (first a-seq) (rest a-seq))))]
+    (last' nil a-seq)))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (let [s-eq (fn [s1 s2]
+               (cond
+                 (and (empty? s1) (empty? s2)) true
+                 (not= (first s1) (first s2)) false
+                 (or (empty? s1) (empty? s2)) false ; to handle a case when one is empty another one has nil
+                 :else (recur (rest s1) (rest s2))))]
+    (s-eq seq1 seq2)))
 
 (defn find-first-index [pred a-seq]
-  ":(")
+  (loop [idx 0 ; initial values, new ones are assigned by recur call
+         sq a-seq]
+    (cond
+      (empty? sq) nil
+      (pred (first sq)) idx
+      :else (recur (inc idx) (rest sq))))) ; recur ~ @tailrec in scala
 
 (defn avg [a-seq]
-  -1)
+  (loop [amount 0
+         a-sum 0
+         sq a-seq]
+    (if (empty? sq)
+      (/ a-sum amount)
+      (recur (inc amount) (+ a-sum (first sq)) (rest sq)))))
 
 (defn parity [a-seq]
-  ":(")
+  (let [toggle (fn [a-set elem]
+                 (if (contains? a-set elem)
+                   (disj a-set elem)
+                   (conj a-set elem)))]
+    (loop [accum #{}
+           sq a-seq]
+      (if (empty? sq)
+        accum
+        (recur (toggle accum (first sq)) (rest sq))))))
 
 (defn fast-fibo [n]
-  ":(")
+  (loop [current-n 1
+         f-n-1 0
+         f-n 1]
+    (if (< current-n n)
+      (recur (inc current-n) f-n (+ f-n-1 f-n))
+      f-n)))
 
 (defn cut-at-repetition [a-seq]
-  [":("])
+  (loop [already-met #{}
+         accum []
+         sq a-seq]
+    (if (or (empty? sq) (contains? already-met (first sq)))
+      accum
+      (recur
+        (conj already-met (first sq))
+        (conj accum (first sq))
+        (rest sq)))))
 
